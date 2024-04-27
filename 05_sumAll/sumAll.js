8de99eb5d8c74c784f44dc60b1603d81ad6d90c3
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2024-04-27 14:58:13 -0400

    Solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let array = [a, b];
+    if ((Number.isInteger(a) && a > 0) && (Number.isInteger(b) && b > 0)) {
+        let arr = array.toSorted();
+        let sum = 0;
+        a = arr[0];
+        b = arr[1];
+        for (a; b >= a; b--) {
+            sum = sum + b;
+        }
+        return sum;
+    }
+    
+    else {
+        return 'ERROR'
+    };
 };
 
 // Do not edit below this line
