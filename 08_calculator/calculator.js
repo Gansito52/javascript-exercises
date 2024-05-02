3b42eab7fd9223222cead9a140b0540f3bcf899e
Gansito52 <68562890+Gansito52@users.noreply.github.com>
2024-05-02 07:50:21 -0400

    Solve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
+
+  else {
+    let arr_sum = 0
+    for (i = 0; i < array.length; i++) {
+      arr_sum = arr_sum + array[i];
+    };
+    return arr_sum;
+  };
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (i = 0; i < arr.length; i++) {
+    product = product*arr[i];
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let product = 1;
+	for (i = 0; i < b; i++) {
+    product = product*a;
+  };
+  return product;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let product = 1;
+
+  for (i = a; i > 0; i--) {
+    product = product*i;
+  };
+
+  return product;
 	
 };
 
